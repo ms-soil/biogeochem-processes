90c2f817300e6c3ca362639972e0f2705bc1c17b
ms-soil <mschmidh@gwdg.de>
2021-01-13 18:45:25 +0100

    added jan 13 2021

--- course-script-jan-13-2021.R
+++ course-script-jan-13-2021.R
@@ -40,6 +40,86 @@ TukeyHSD(aov(d$co2flux~d$landuse))
 
 
 
+#### showing and ordering means ####
+
+means <- d %>% group_by(landuse) %>% summarize(m = mean(co2flux)) %>% arrange(-m)
+means
+
+#### function to test automatically: ####
+
+
+MS_mg<-function(dataframe, parameter, treatment) {
+  cat("\014") # clear console
+  #test equality of variances
+  p_equal_v<-leveneTest(dataframe[,parameter]~dataframe[,treatment])
+  print("Equality of variance test returns:")
+  print(p_equal_v[1,3])
+  
+  if(p_equal_v[1,3]<=0.05) {
+    result1<-oneway.test(dataframe[,parameter]~dataframe[,treatment], data = dataframe, var.equal=FALSE)$p.value
+    #summary1<-summary(oneway.test(dataframe[,parameter]~dataframe[,treatment], data = dataframe, var.equal=FALSE))
+    print("The final one-way test returns:")
+    print(result1)
+    #print(summary1)
+    print("Here are the test statistics:")
+    print(oneway.test(dataframe[,parameter]~dataframe[,treatment], data = dataframe, var.equal=FALSE))
+    
+    if(result1<=0.05)
+    {
+      print("A post-hoc test is done:")
+      y<-dataframe[,parameter]
+      x<-dataframe[,treatment]
+      pairwise.t.test(y, x, pool.sd=FALSE, paired=FALSE, p.adjust.method='holm')
+    }
+    else
+    {
+      print("no post-hoc test is done")
+    }
+  }
+  else
+  {
+    print("Equal variances are assumed.")
+    print("Please check if this is logical.")
+    p_normality<-shapiro.test(residuals(lm(dataframe[,parameter]~dataframe[,treatment])))$p.value
+    print("Normality of residuals returns:")
+    print(p_normality)
+    
+    if(p_normality>=0.05)
+    {
+      model1<-aov(dataframe[,parameter]~dataframe[,treatment])
+      result2<-summary(model1)[[1]][["Pr(>F)"]][[1]]
+      print("The final ANOVA returns:")
+      print(result2)
+      print("Here are the test statistics:")
+      print(summary(model1))
+      if(result2<=0.05)
+      {TukeyHSD(model1)}
+      else
+      {print("No post-hoc test is done")}
+      
+    }
+    else
+    {
+      result3<-kruskal.test((dataframe[,parameter]~dataframe[,treatment]))$p.value
+      print("The final Kruskal-Wallis test returns:")
+      print(result3)
+      print(kruskal.test((dataframe[,parameter]~dataframe[,treatment])))
+      if(result3<=0.05)
+      {kruskalmc(dataframe[,parameter]~dataframe[,treatment], probs=0.05, cont=NULL)}
+      else
+      {print("No post-hoc test is done")}
+    }
+    
+  }
+}
+
+MS_mg(d, 'co2flux', 'landuse')
+MS_mg(d, 'n2oflux', 'landuse')
+MS_mg(d, 'ch4flux', 'landuse')
+MS_mg(d, 'soc', 'landuse')
+
+
+
 
 
 
